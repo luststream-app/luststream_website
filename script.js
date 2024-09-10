9d7e056b9f21a04a6894e551c6ae085da206d544
luststream-app <app.luststream@gmail.com>
2024-09-11 02:00:10 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+// script.js
+
+document.addEventListener("DOMContentLoaded", () => {
+  const menuToggle = document.querySelector(".menu-toggle");
+  const navLinks = document.querySelector(".nav-links");
+
+  menuToggle.addEventListener("click", () => {
+    navLinks.classList.toggle("active");
+  });
+});
+
+document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+  anchor.addEventListener("click", function (e) {
+    e.preventDefault();
+    document.querySelector(this.getAttribute("href")).scrollIntoView({
+      behavior: "smooth",
+    });
+  });
+});
